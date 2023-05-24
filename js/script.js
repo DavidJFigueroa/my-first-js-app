cfe6e3d6cc1d05bdb3467f5a1ab367730c8f9cc8
David Figueroa <122026800+DavidJFigueroa@users.noreply.github.com>
2023-05-24 14:18:19 +0200

    feat:for loop added

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let pokemonList = [
   }, 
   {
     name: 'Nidoran',
-    height: 0.4,
+    height: 1.4,
     types: ['poison'],
   }, 
   {
@@ -15,3 +15,12 @@ let pokemonList = [
     types: ['poison'],
   }, 
 ];
+
+for (let i = 0; i < pokemonList.length; i++) { 
+  if (pokemonList[i].height >= 2) {
+    document.write(pokemonList[i].name + " " + "\(height: " + pokemonList[i].height + "\)" + " - Wow, that’s big!" + "<br>");
+  } else {
+    document.write(pokemonList[i].name + " " + "\(height: " + pokemonList[i].height + "\)" + "<br>");
+  }
+}
+
