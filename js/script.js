1b374cd955bb5eb16b39bb277f0cefe758e2fa2f
David Figueroa <122026800+DavidJFigueroa@users.noreply.github.com>
2023-05-24 10:52:35 +0200

    feat: pokemonList[] added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'Kimchi';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    types: ['water'],
+  }, 
+  {
+    name: 'Nidoran',
+    height: 0.4,
+    types: ['poison'],
+  }, 
+  {
+    name: 'Ekans',
+    height: 2,
+    types: ['poison'],
+  }, 
+]
