fcbde93207d1e9332851cb2e887c6d801673a6ad
David Figueroa <122026800+DavidJFigueroa@users.noreply.github.com>
2023-06-21 11:34:13 +0200

    feat:simplepagination.js added

--- src/js/script.js
+++ src/js/script.js
@@ -146,166 +146,323 @@ $(document).ready(function () {
 
 //pagination not working
 
+$(document).ready(function () {
+  var items = $(".list-group .list-group-item");
+
+  var numItems = items.length;
+  var perPage = 30;
+
+  console.log(items);
+  console.log(numItems);
+
+  items.slice(perPage).hide();
+
+  $(function () {
+    $("#pagination-container").pagination({
+      items: numItems,
+      itemsOnPage: perPage,
+      cssStyle: "light-theme",
+    });
+  });
+});
+
 // $("#pagination-container").pagination({
-//   dataSource: pokemonRepository.addListItem(pokemon),
+//   dataSource: function () {
+//     console.log(pokemonRepository.getAll());
+//     pokemonRepository.getAll();
+//   },
 //   pageSize: 5,
 //   autoHidePrevious: true,
 //   autoHideNext: true,
 //   callback: function (data, pagination) {
-//     // template method of yourself
-//     var html = template(data);
+//     var html =
+//       "<ul>" + data.map((pokemon) => "<li>${pokemon}</li>").join("") + "</ul>";
 //     $("#data-container").html(html);
 //   },
 // });
 
-(function ($) {
-  var paginate = {
-    startPos: function (pageNumber, perPage) {
-      // determine what array position to start from
-      // based on current page and # per page
-      return pageNumber * perPage;
-    },
+// $("#pagination-container").pagination({
+//   dataSource: function () {
+//     console.log(pokemonRepository.getAll());
+//     pokemonRepository.getAll();
+//   },
+//   pageSize: 5,
+//   autoHidePrevious: true,
+//   autoHideNext: true,
+//   callback: function (response, pagination) {
+//     console.log(response, pagination);
+//     var html =
+//       "<ul>" +
+//       response.map((pokemon) => "<li>${pokemon}</li>").join("") +
+//       "</ul>";
+//     $("#data-container").html(html);
+//   },
+// });
 
-    getPage: function (items, startPos, perPage) {
-      // declare an empty array to hold our page items
-      var page = [];
+// $(function () {
+//   (function (name) {
+//     var container = $("#pagination-" + name);
+//     if (!container.length) return;
+//     // var sources = (function () {
+//     //   pokemonRepository.loadList().then(function () {
+//     //     pokemonRepository.sortByName();
+//     //     pokemonRepository.getAll().forEach(function (pokemon) {
+//     //       pokemonRepository.addListItem(pokemon);
+//     //     });
+//     //   });
+//     // })();
 
-      // only get items after the starting position
-      items = items.slice(startPos, items.length);
+//     var options = {
+//       dataSource: function () {
+//         pokemonRepository.loadList().then(function () {
+//           pokemonRepository.sortByName();
+//           pokemonRepository.getAll().forEach(function (pokemon) {
+//             pokemonRepository.addListItem(pokemon);
+//           });
+//         });
+//       },
+//       callback: function (response, pagination) {
+//         window.console && console.log(response, pagination);
 
-      // loop remaining items until max per page
-      for (var i = 0; i < perPage; i++) {
-        page.push(items[i]);
-      }
+//         var dataHtml = "<ul>";
 
-      return page;
-    },
+//         $.each(response, function (index, item) {
+//           dataHtml += "<li>" + item + "</li>";
+//         });
 
-    totalPages: function (items, perPage) {
-      // determine total number of pages
-      return Math.ceil(items.length / perPage);
-    },
+//         dataHtml += "</ul>";
 
-    createBtns: function (totalPages, currentPage) {
-      // create buttons to manipulate current page
-      var pagination = $('<div class="pagination" />');
+//         container.prev().html(dataHtml);
+//       },
+//     };
 
-      // add a "first" button
-      pagination.append('<span class="pagination-button">&laquo;</span>');
+//     //$.pagination(container, options);
 
-      // add pages inbetween
-      for (var i = 1; i <= totalPages; i++) {
-        // truncate list when too large
-        if (totalPages > 5 && currentPage !== i) {
-          // if on first two pages
-          if (currentPage === 1 || currentPage === 2) {
-            // show first 5 pages
-            if (i > 5) continue;
-            // if on last two pages
-          } else if (
-            currentPage === totalPages ||
-            currentPage === totalPages - 1
-          ) {
-            // show last 5 pages
-            if (i < totalPages - 4) continue;
-            // otherwise show 5 pages w/ current in middle
-          } else {
-            if (i < currentPage - 2 || i > currentPage + 2) {
-              continue;
-            }
-          }
-        }
+//     container.addHook("beforeInit", function () {
+//       window.console && console.log("beforeInit...");
+//     });
+//     container.pagination(options);
 
-        // markup for page button
-        var pageBtn = $('<span class="pagination-button page-num" />');
+//     container.addHook("beforePageOnClick", function () {
+//       window.console && console.log("beforePageOnClick...");
+//       //return false
+//     });
+//   })("container");
+// });
 
-        // add active class for current page
-        if (i == currentPage) {
-          pageBtn.addClass("active");
-        }
+// $(function () {
+//   (function () {
+//     var container = $("#pagination-container");
+//     if (!container.length) return;
+//     container.pagination({
+//       dataSource: function () {
+//         pokemonRepository.getAll();
+//       },
+//       pageNumber: 2,
+//       totalNumber: 120,
+//       pageSize: 20,
+//       ajax: {
+//         beforeSend: function () {
+//           container.prev().html("Loading data from flickr.com ...");
+//         },
+//       },
+//       callback: function (response, pagination) {
+//         window.console && console.log(22, response, pagination);
+//         var dataHtml = "<ul>";
 
-        // set text to the page number
-        pageBtn.text(i);
+//         $.each(response, function (index, item) {
+//           dataHtml += "<li>" + item.title + "</li>";
+//         });
 
-        // add button to the container
-        pagination.append(pageBtn);
-      }
+//         dataHtml += "</ul>";
 
-      // add a "last" button
-      pagination.append($('<span class="pagination-button">&raquo;</span>'));
+//         container.prev().html(dataHtml);
+//       },
+//     });
+//   })();
+// });
 
-      return pagination;
-    },
+//   //$.pagination(container, options);
 
-    createPage: function (items, currentPage, perPage) {
-      // remove pagination from the page
-      $(".pagination").remove();
+//   container.addHook("beforeInit", function () {
+//     window.console && console.log("beforeInit...");
+//   });
+//   container.pagination(options);
 
-      // set context for the items
-      var container = items.parent(),
-        // detach items from the page and cast as array
-        items = items.detach().toArray(),
-        // get start position and select items for page
-        startPos = this.startPos(currentPage - 1, perPage),
-        page = this.getPage(items, startPos, perPage);
+//   container.addHook("beforePageOnClick", function () {
+//     window.console && console.log("beforePageOnClick...");
+//     //return false
+//   });
+// })("container1");
 
-      // loop items and readd to page
-      $.each(page, function () {
-        // prevent empty items that return as Window
-        if (this.window === undefined) {
-          container.append($(this));
-        }
-      });
+// (function ($) {
+//   var paginate = {
+//     startPos: function (pageNumber, perPage) {
+//       // determine what array position to start from
+//       // based on current page and # per page
+//       return pageNumber * perPage;
+//     },
 
-      // prep pagination buttons and add to page
-      var totalPages = this.totalPages(items, perPage),
-        pageButtons = this.createBtns(totalPages, currentPage);
+//     getPage: function (items, startPos, perPage) {
+//       // declare an empty array to hold our page items
+//       var page = [];
 
-      container.after(pageButtons);
-    },
-  };
+//       // only get items after the starting position
+//       items = items.slice(startPos, items.length);
 
-  // stuff it all into a jQuery method!
-  $.fn.paginate = function (perPage) {
-    var items = $(this);
+//       // loop remaining items until max per page
+//       for (var i = 0; i < perPage; i++) {
+//         page.push(items[i]);
+//       }
 
-    // default perPage to 5
-    if (isNaN(perPage) || perPage === undefined) {
-      perPage = 5;
-    }
+//       return page;
+//     },
 
-    // don't fire if fewer items than perPage
-    if (items.length <= perPage) {
-      return true;
-    }
+//     totalPages: function (items, perPage) {
+//       // determine total number of pages
+//       return Math.ceil(items.length / perPage);
+//     },
 
-    // ensure items stay in the same DOM position
-    if (items.length !== items.parent()[0].children.length) {
-      items.wrapAll('<div class="pagination-items" />');
-    }
+//     createBtns: function (totalPages, currentPage) {
+//       // create buttons to manipulate current page
+//       var pagination = $('<div class="pagination" />');
 
-    // paginate the items starting at page 1
-    paginate.createPage(items, 1, perPage);
+//       // add a "first" button
+//       pagination.append('<span class="pagination-button">&laquo;</span>');
 
-    // handle click events on the buttons
-    $(document).on("click", ".pagination-button", function (e) {
-      // get current page from active button
-      var currentPage = parseInt($(".pagination-button.active").text(), 10),
-        newPage = currentPage,
-        totalPages = paginate.totalPages(items, perPage),
-        target = $(e.target);
+//       // add pages inbetween
+//       for (var i = 1; i <= totalPages; i++) {
+//         // truncate list when too large
+//         if (totalPages > 5 && currentPage !== i) {
+//           // if on first two pages
+//           if (currentPage === 1 || currentPage === 2) {
+//             // show first 5 pages
+//             if (i > 5) continue;
+//             // if on last two pages
+//           } else if (
+//             currentPage === totalPages ||
+//             currentPage === totalPages - 1
+//           ) {
+//             // show last 5 pages
+//             if (i < totalPages - 4) continue;
+//             // otherwise show 5 pages w/ current in middle
+//           } else {
+//             if (i < currentPage - 2 || i > currentPage + 2) {
+//               continue;
+//             }
+//           }
+//         }
 
-      // get numbered page
-      newPage = parseInt(target.text(), 10);
-      if (target.text() == "«") newPage = 1;
-      if (target.text() == "»") newPage = totalPages;
+//         // markup for page button
+//         var pageBtn = $('<span class="pagination-button page-num" />');
 
-      // ensure newPage is in available range
-      if (newPage > 0 && newPage <= totalPages) {
-        paginate.createPage(items, newPage, perPage);
-      }
-    });
-  };
-})(jQuery);
+//         // add active class for current page
+//         if (i == currentPage) {
+//           pageBtn.addClass("active");
+//         }
 
-$(".list-group-item").paginate(20);
+//         // set text to the page number
+//         pageBtn.text(i);
+
+//         // add button to the container
+//         pagination.append(pageBtn);
+//       }
+
+//       // add a "last" button
+//       pagination.append($('<span class="pagination-button">&raquo;</span>'));
+
+//       return pagination;
+//     },
+
+//     createPage: function (items, currentPage, perPage) {
+//       // remove pagination from the page
+//       $(".pagination").remove();
+
+//       // set context for the items
+//       var container = items.parent(),
+//         // detach items from the page and cast as array
+//         items = items.detach().toArray(),
+//         // get start position and select items for page
+//         startPos = this.startPos(currentPage - 1, perPage),
+//         page = this.getPage(items, startPos, perPage);
+
+//       // loop items and readd to page
+//       $.each(page, function () {
+//         // prevent empty items that return as Window
+//         if (this.window === undefined) {
+//           container.append($(this));
+//         }
+//       });
+
+//       // prep pagination buttons and add to page
+//       var totalPages = this.totalPages(items, perPage),
+//         pageButtons = this.createBtns(totalPages, currentPage);
+
+//       container.after(pageButtons);
+//     },
+//   };
+
+//   // stuff it all into a jQuery method!
+//   $.fn.paginate = function (perPage) {
+//     var items = $(this);
+
+//     // default perPage to 5
+//     if (isNaN(perPage) || perPage === undefined) {
+//       perPage = 5;
+//     }
+
+//     // don't fire if fewer items than perPage
+//     if (items.length <= perPage) {
+//       return true;
+//     }
+
+//     // ensure items stay in the same DOM position
+//     if (items.length !== items.parent()[0].children.length) {
+//       items.wrapAll('<div class="pagination-items" />');
+//     }
+
+//     // paginate the items starting at page 1
+//     paginate.createPage(items, 1, perPage);
+
+//     // handle click events on the buttons
+//     $(document).on("click", ".pagination-button", function (e) {
+//       // get current page from active button
+//       var currentPage = parseInt($(".pagination-button.active").text(), 10),
+//         newPage = currentPage,
+//         totalPages = paginate.totalPages(items, perPage),
+//         target = $(e.target);
+
+//       // get numbered page
+//       newPage = parseInt(target.text(), 10);
+//       if (target.text() == "«") newPage = 1;
+//       if (target.text() == "»") newPage = totalPages;
+
+//       // ensure newPage is in available range
+//       if (newPage > 0 && newPage <= totalPages) {
+//         paginate.createPage(items, newPage, perPage);
+//       }
+//     });
+//   };
+// })(jQuery);
+
+// $(".list-group-item").paginate(20);
+
+// var items = $(".list-group .list-group-item");
+
+// var items = $(".list-group .list-group-item");
+
+// var numItems = items.length;
+// var perPage = 30;
+
+// console.log(items);
+// console.log(numItems);
+
+// items.slice(perPage).hide();
+
+// $(function () {
+//   $("#pagination-container").pagination({
+//     items: numItems,
+//     itemsOnPage: perPage,
+//     cssStyle: "light-theme",
+//   });
+// });
